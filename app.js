2cc27f0617731156a597ec3464031c422cb2c1c7
Prince Kumar Singh <138196541+prince-kumar-singh@users.noreply.github.com>
2025-02-16 15:27:18 +0530

    initialize project with Express and Socket.io for chess game

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const socket = require('socket.io');
+const http = require('http');
+const {Chess} = require('chess');
+
+const app = express();
+
+const server = http.createServer(app);
+const io = socket(server);
+
+const chess = new Chess();
