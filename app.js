8cd2745ba47eb82b7119c7b4c1a0c730f4d99504
Prince Kumar Singh <138196541+prince-kumar-singh@users.noreply.github.com>
2025-02-16 15:41:48 +0530

    add title to index view for chess game

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index',{title: 'Chess Game'});
 });
 
 server.listen(3000, function () {
